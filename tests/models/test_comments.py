207e288c0beacd917087a7a430c3fe1e496a9912
Asuka Toyoshima <asuka.toyoshima@di-v.co.jp>
2024-07-24 11:19:43 +0900

    コメントのテストコード実装準備

--- tests/models/test_comments.py
+++ tests/models/test_comments.py
@@ -1,5 +1,14 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
-
+from ..factories.users import UserFactory
+from ..factories.tweets import TweetFactory
+from ..factories.comments import CommentFactory
 
 class CommentModelTestCase(TestCase):
+    def setUp(self):
+        self.user = UserFactory.create()
+        self.tweet = TweetFactory.create(user=self.user)
+        self.comment = CommentFactory.build(user=self.user,tweet=self.tweet)
+
+    def test_text_cannot_be_blank(self):
+        # ここに問題2のテストコードを記述
